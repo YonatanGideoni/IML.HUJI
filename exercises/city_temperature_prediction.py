bd97aec760689b60f75dff04f93f854c77442eda
Yonatan Gideoni <yonatan.gideoni@gmail.com>
2022-03-31 12:03:21 +0300

    Implemented load_data

--- exercises/city_temperature_prediction.py
+++ exercises/city_temperature_prediction.py
@@ -1,11 +1,7 @@
-import IMLearn.learners.regressors.linear_regression
-from IMLearn.learners.regressors import PolynomialFitting
-from IMLearn.utils import split_train_test
-
 import numpy as np
 import pandas as pd
-import plotly.express as px
 import plotly.io as pio
+
 pio.templates.default = "simple_white"
 
 
@@ -21,13 +17,21 @@ def load_data(filename: str) -> pd.DataFrame:
     -------
     Design matrix and response vector (Temp)
     """
-    raise NotImplementedError()
+    data = pd.read_csv(filename, parse_dates=['Date'])
+
+    # remove absurdly low temperatures
+    MIN_TEMP = -40
+    data = data[data.Temp > MIN_TEMP]
+
+    data['DayOfYear'] = data.Date.dt.dayofyear
+
+    return data
 
 
 if __name__ == '__main__':
     np.random.seed(0)
     # Question 1 - Load and preprocessing of city temperature dataset
-    raise NotImplementedError()
+    data = load_data('../datasets/City_Temperature.csv')
 
     # Question 2 - Exploring data for specific country
     raise NotImplementedError()
