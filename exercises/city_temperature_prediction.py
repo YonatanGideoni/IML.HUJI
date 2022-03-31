a79244a51468bb534617cf2807c0248e88b15687
Yonatan Gideoni <yonatan.gideoni@gmail.com>
2022-03-31 12:44:17 +0300

    Finished Q2

--- exercises/city_temperature_prediction.py
+++ exercises/city_temperature_prediction.py
@@ -1,6 +1,9 @@
+from itertools import cycle
+
 import numpy as np
 import pandas as pd
 import plotly.io as pio
+from matplotlib import pyplot as plt
 
 pio.templates.default = "simple_white"
 
@@ -28,19 +31,45 @@ def load_data(filename: str) -> pd.DataFrame:
     return data
 
 
+def plot_temp_per_year_day(data: pd.DataFrame, country='Israel'):
+    country_data = data[data.Country == country]
+
+    cycol = cycle(list('bgrcmk') + ['orange', 'pink', 'goldenrod', 'darkblue', 'lime', 'dimgray', 'brown'])
+
+    country_data.groupby('Year') \
+        .apply(lambda df: df.plot.scatter(x='DayOfYear', y='Temp', label=df.name, ax=plt.gca(), grid=True,
+                                          c=next(cycol)))
+
+    plt.legend(fontsize=12, ncol=5)
+    plt.xlabel('Day of year', fontsize=14)
+    plt.ylabel('Temperature[C]', fontsize=14)
+    plt.title('Temperature vs. day of year for different years', fontsize=16)
+    MAX_DAY_OF_YEAR = 366
+    plt.xlim(0, MAX_DAY_OF_YEAR)
+
+    country_data.groupby('Month').Temp.agg(['mean', 'std']).reset_index().plot.bar(x='Month', y='mean', yerr='std')
+
+    plt.ylabel('Temperature', fontsize=14)
+    plt.xlabel('Month', fontsize=14)
+    plt.title('Average temperature at each month', fontsize=16)
+    plt.gca().get_legend().remove()
+
+
 if __name__ == '__main__':
     np.random.seed(0)
     # Question 1 - Load and preprocessing of city temperature dataset
     data = load_data('../datasets/City_Temperature.csv')
 
     # Question 2 - Exploring data for specific country
-    raise NotImplementedError()
+    plot_temp_per_year_day(data)
 
     # Question 3 - Exploring differences between countries
-    raise NotImplementedError()
+    # raise NotImplementedError()
 
     # Question 4 - Fitting model for different values of `k`
-    raise NotImplementedError()
+    # raise NotImplementedError()
 
     # Question 5 - Evaluating fitted model on different countries
-    raise NotImplementedError()
+    # raise NotImplementedError()
+
+    plt.show()
