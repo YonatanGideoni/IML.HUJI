2dd40b0e788660f0bcb919e93a92e535323a6017
Yonatan Gideoni <yonatan.gideoni@mail.huji.ac.il>
2022-05-26 09:57:43 +0300

    Implemented cross_validate

--- IMLearn/model_selection/cross_validate.py
+++ IMLearn/model_selection/cross_validate.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
-from copy import deepcopy
+
 from typing import Tuple, Callable
+
 import numpy as np
+
 from IMLearn import BaseEstimator
 
 
@@ -37,4 +39,22 @@ def cross_validate(estimator: BaseEstimator, X: np.ndarray, y: np.ndarray,
     validation_score: float
         Average validation score over folds
     """
-    raise NotImplementedError()
+    folds = np.arange(len(y)) // (len(y) / cv)
+    np.random.shuffle(folds)
+    train_scores = np.zeros(cv)
+    validation_scores = np.zeros(cv)
+
+    for validation_fold in range(cv):
+        fold_train_X = X[folds != validation_fold]
+        fold_train_y = y[folds != validation_fold]
+
+        fold_estimator = estimator.fit(fold_train_X, fold_train_y)
+
+        train_scores[validation_fold] = scoring(fold_estimator.predict(fold_train_X), fold_train_y)
+
+        fold_val_X = X[folds == validation_fold]
+        fold_val_y = y[folds == validation_fold]
+
+        validation_scores[validation_fold] = scoring(fold_estimator.predict(fold_val_X), fold_val_y)
+
+    return train_scores.mean(), validation_scores.mean()
